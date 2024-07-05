7eaa9950b9fe67d2b42281c16a0d7376f053e6ba
someoneisanna <annacarolinataboada@gmail.com>
2024-07-05 10:10:50 +0100

    added microshell

--- rank_4/answers/microshell.c
+++ rank_4/answers/microshell.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <sys/wait.h>
 
-void error(char *msg, char *arg)
+int error(char *msg, char *arg)
 {
 	for (int i = 0; msg[i]; i++)
 		write(2, &msg[i], 1);
@@ -14,39 +14,63 @@ void error(char *msg, char *arg)
 			write(2, &arg[i], 1);
 	}
 	write(2, "\n", 1);
-	exit (1);
+	return (1);
 }
 
-int execute_cd(int index, char **av)
+int execute_cd(int n_args, char **args)
 {
-	if (!av[index + 1] || strcmp(av[index + 2], ";"))
-		error("error: cd: bad arguments", NULL);
-	if (chdir(av[2]) < 0)
-		error("error: cd: cannot change directory to ", av[2]);
-	return (index + 2);
+	if (n_args != 2)
+		return (error("error: cd: bad arguments", NULL));
+	if (chdir(args[1]) < 0)
+		return (error("error: cd: cannot change directory to ", args[1]));
+	return (0);
 }
 
-int execute_cmd(int index, char **av, char **envp)
+int execute_cmd(int end, char **args, char **envp)
 {
-	(void)envp;
-	printf("execute_cmd: %s\n", av[index]);
-	return (index);
+	int fd[2];
+	int exit_code;
+	int has_pipe = 0;
+	int pid;
+	
+	if (end == 0)
+		return (1);
+	if (args[end] && !strcmp(args[end], "|"))
+		has_pipe = 1;
+	if (has_pipe && pipe(fd) < 0)
+		return (error("error: fatal", NULL));
+	pid = fork();
+	if (pid == 0)
+	{
+		args[end] = NULL;
+		if (has_pipe && (dup2(fd[1], 1) < 0 || close(fd[0]) < 0 || close(fd[1]) < 0))
+			return (error("error: fatal", NULL));
+		execve(args[0], args, envp);
+		return (error("error: cannot execute ", args[0]));
+	}
+	waitpid(pid, &exit_code, 0);
+	if (has_pipe && (dup2(fd[0], 0) < 0 || close(fd[0]) < 0 || close(fd[1]) < 0))
+		return (error("error: fatal", NULL));
+	return (WEXITSTATUS(exit_code));
 }
 
 int main(int ac, char **av, char **envp)
 {
-	(void)envp;
+	int i = 0;
+	int start = 0;
+	int exit_code = 0;
+
 	if (ac < 2)
 		return (1);
-	for (int i = 1; av[i]; i++)
+	while (av[i] && av[++i])
 	{
-		if (!strcmp(av[i], "cd"))
-			i = execute_cd(i, av);
-		else if (!strcmp(av[i], ";"))
-			continue;
-		else if (!strcmp(av[i], "|"))
-			i = execute_cmd(i, av, envp);
+		start = i;
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+			i++;
+		if (!strcmp(av[start], "cd"))
+			exit_code = execute_cd(i - start, av + start);
 		else
-			printf("%s\n", av[i]);
+			exit_code = execute_cmd(i - start, av + start, envp);
 	}
+	return (exit_code);
 }
